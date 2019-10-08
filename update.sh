5982943a241c51dc7c5be0b8a2d6eff989c5cd0e
Bjornskjald <github@bjorn.ml>
2019-10-08 19:22:10 +0200

    added updating nginx without restarting

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull && python3 generate.py -o ./nginx && docker-compose up -d --force-recreate
+git pull && python3 generate.py -o ./nginx && docker-compose kill -s SIGHUP nginx || docker-compose up -d --force-recreate
