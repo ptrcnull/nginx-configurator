c93ff5c48b6b12d769920d503400fc2c9cea1aa1
ptrcnull <git@ptrcnull.me>
2020-01-12 13:40:37 +0100

    made main module programmable

--- generate.py
+++ generate.py
@@ -7,13 +7,8 @@ from generator.config import Config
 from generator.templates import server, server_http
 import argparse
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate Nginx configuration.')
-    parser.add_argument('-o', '--out', default='./nginx', metavar='path',
-                        help='Output directory (default: current directory)')
-    args = parser.parse_args()
-    # print(args)
 
+def generate(args):
     os.makedirs(args.out, exist_ok=True)
 
     certificates = get_certificates()
@@ -35,3 +30,13 @@ if __name__ == '__main__':
 
         with open(path.join(args.out, f'{host}.conf'), 'w') as f:
             f.write(output)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate Nginx configuration.')
+    parser.add_argument('-o', '--out', default='./nginx', metavar='path',
+                        help='Output directory (default: current directory)')
+    _args = parser.parse_args()
+    generate(_args)
+    # print(args)
+
